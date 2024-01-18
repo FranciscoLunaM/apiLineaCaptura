f211870fad30c3d7d4a02484953b2f8d802649e0
FranciscoLunaM <franciscoe.martinez@hotmail.com>
2024-01-18 08:16:12 -0600

    Version estable 1.0
    Esta es la version mas actualizada de la api

--- app.py
+++ app.py
@@ -13,15 +13,14 @@ from waitress import serve
 import consul
 import logging
 import json
-from fpdf import FPDF
+from fpdf import FPDF, YPos
 from flask_cors import CORS
 import qrcode
 from barcode import Code128
 from barcode.writer import SVGWriter
 ssl._create_default_https_context = ssl._create_unverified_context
-
-#from database import obtenerConexion
-#conexion= obtenerConexion()
+from database import obtenerConexion
+conexion= obtenerConexion()
 
 #Get all variables from env
 load_dotenv()
@@ -54,7 +53,7 @@ s.close()
 
 
 #consul Config
-"""
+
 c = consul.Consul(host=consulIp, port=consulPort)
 c.agent.service.register('servicio-linea-captura',
                         service_id='servicio-linea-captura',
@@ -62,7 +61,6 @@ c.agent.service.register('servicio-linea-captura',
                         address=ip_address,
                         tags=['servicio-linea-captura'])
 
-"""
 
 
 app = Flask(__name__)
@@ -74,14 +72,43 @@ def consultar_linea_captura():
         user = request_data['user']
         email = request_data['email']
         folio=request_data['folio']
+        clienteID=request_data['clientId']
         idTramite=request_data['idTramite']
-   
+        hora = datetime.datetime.now()
+
         data=json.dumps({"user":user, "email":email}, separators=(',', ':'))
         
         tokenGenerado=generarToken(data)
         
         lineaDeCaptura=solicitar_linea_captura(tokenGenerado,folio,idTramite)
-        #logging.warning("Pruebaaaaaaaaaaaaaaaaaaaa"+lineaDeCaptura["data"]["fechaVencimiento"])
+        
+        
+        if lineaDeCaptura["resultado"]==True:
+            url2="https://enlace-qa.tabasco.gob.mx/api/v1/linea-captura/consulta/documento_linea_captura?folio="+folio
+            logging.warning(url2)
+
+            url=lineaDeCaptura["data"]["urlFormatoPago"]["_text"]
+            logging.warning(url)         
+            today = datetime.date.today()
+            logging.warning(today)
+            year = today.year
+            logging.warning(year)   
+            splitLinea= lineaDeCaptura["data"]["lineaCaptura"]["_text"].split("|", 1)
+            logging.warning(splitLinea)  
+            lineaDeCaptura.update({"urlPdf2":url2})
+            logging.warning("linea final antes--------------")  
+
+            conexion= obtenerConexion()
+            with conexion.cursor() as cursor:
+                
+                sql="insert into pagos_finanzas_pagoreferenciado (contribuyente,folio_seguimiento,importe,fecha_registro,folio_control,linea_captura,fecha_vencimiento,linea_captura_oxxo,estatus,tramite_id,user_id,ejercicio,respuesta_finanzas,url_formato,client_id,url_formato2) values ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}','{13}','{14}','{15}')".format(user,folio,lineaDeCaptura["data"]["importe"]["_text"],hora,lineaDeCaptura["data"]["folioControlEstado"]["_text"],splitLinea[0],lineaDeCaptura["data"]["fechaVencimiento"]["_text"],splitLinea[1],"1",idTramite,"1",year,json.dumps(lineaDeCaptura),url,clienteID,url2)
+                logging.warning(sql)  
+                logging.warning(sql)
+                cursor.execute(sql)
+                conexion.commit()
+                conexion.close()
+                
+        
         return lineaDeCaptura
     
     except Exception as error:
@@ -198,41 +225,55 @@ def consultarPago():
                 'X-SESSION-KEY':xSessionKeyTrue,
                 'Authorization':'Bearer '+ tokenGenerado
             }
-    param=json.dumps({"query":dataEncriptada})
+    
     
     response=requests.get(url=rutaConsultaPago+"?query="+dataEncriptada,headers=header)
     desencriptadoLineaCaptura=desencriptado(response.text)
     dataDesencriptado= json.loads(desencriptadoLineaCaptura)
+
+    #Agregar a la base de datos
+    """if dataDesencriptado["resultado"]==True:
+        conexion= obtenerConexion()
+
+        with conexion.cursor() as cursor:
+            number=int(dataDesencriptado["data"]["codigoEstatus"]["_text"])
+            sql= "update pagos_finanzas_pagoreferenciado set estatus='"+str(number)+"' where folio_seguimiento like '"+folio +"'"
+            cursor.execute(sql)
+            conexion.commit()
+            conexion.close()"""
+
     return dataDesencriptado
 
-@app.route("/documento_linea_captura", methods=["POST"])
+@app.route("/consulta/documento_linea_captura", methods=["GET"])
 def documento_linea_captura():
-    request_data = request.get_json()
-    #contribuyente = request_data['contribuyente']
-    folio = request_data['folio']
-    importe=request_data['importe']
-    controlEstado=request_data['controlEstado']
-    fechaVencimiento=request_data['fechaVencimiento']
-    fechaEmision=request_data['fechaEmision']
-    concepto=request_data['concepto']
-    lineaDeCaptura=request_data['lineaCaptura']
-    splitLinea= lineaDeCaptura.split("|", 1)
-    
-    #with conexion.cursor() as cursor:
-        #sql="insert into pagos_finanzas_pagoreferenciado (contribuyente,folio_seguimiento,importe,fecha_registro,folio_control,linea_captura,fecha_vencimiento,linea_captura_oxxo,estatus,tramite_id,user_id) values ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}')".format(contribuyente,folio,importe,fechaEmision,controlEstado,splitLinea[0],fechaVencimiento,splitLinea[1],"2","1","1")
-        #cursor.execute(sql)
-        #conexion.commit()
-    return generarPDF(folio,importe,controlEstado,fechaEmision,fechaVencimiento,concepto,splitLinea)
+    request_data = request.args.get("folio")
+    conexion= obtenerConexion()
+    with conexion.cursor() as cursor:
+        sql="select pf.folio_seguimiento,pf.importe,pf.folio_control,pf.fecha_registro,pf.fecha_vencimiento,pt.nombre,pf.linea_captura ,pf.linea_captura_oxxo from pagos_finanzas_pagoreferenciado as pf, pagos_finanzas_tramite as pt where folio_seguimiento = '"+request_data+"' and pf.tramite_id=pt.id "
+        cursor.execute(sql)
+        data=cursor.fetchone()
+        folio=str(data[0])
+        importe=str(data[1])
+        controlEstado=str(data[2])
+        fechaEmision=str(data[3])
+        fechaVencimiento=str(data[4])
+        concepto=str(data[5])
+        lineaCaptura=data[6]
+        lineaCapturaOxxo=data[7]
+        conexion.close()
+        
+      
+    return generarPDF(folio,importe,controlEstado,fechaEmision,fechaVencimiento,concepto,lineaCaptura,lineaCapturaOxxo)
     
     
 
-def generarPDF(folio,importe,controlEstado,fechaEmision,fechaVencimiento,concepto,splitLinea):
+def generarPDF(folio,importe,controlEstado,fechaEmision,fechaVencimiento,concepto,lineaCaptura,lineaCapturaOxxo):
     
     hora = datetime.datetime.now().time()
     pdf = PDF()
     pdf.set_auto_page_break(auto=True,margin=15)
     pdf.add_page()
-    pdf.body(folio,importe,controlEstado,fechaEmision,fechaVencimiento,hora,concepto,splitLinea)
+    pdf.body(folio,importe,controlEstado,fechaEmision,fechaVencimiento,hora,concepto,lineaCaptura,lineaCapturaOxxo)
     #Creacion del PDF
     stream = pdf.output(dest='S')
     return send_file(io.BytesIO(stream),download_name='logo.pdf')
@@ -241,7 +282,7 @@ def generarPDF(folio,importe,controlEstado,fechaEmision,fechaVencimiento,concept
 class PDF(FPDF):
     def header(self):
         #apilineacaptura/images/tb.png
-        self.image("./images/tb.png",20,15,20)
+        self.image("apilineacaptura/images/tb.png",20,15,20)
         self.ln(5)
         self.set_font('helvetica', size=9)
         self.cell(0,5, "GOBIERNO DEL ESTADO DE TABASCO",ln=1,align="C")
@@ -250,23 +291,24 @@ class PDF(FPDF):
 #Fin del Header
 #footer PDF
     def footer(self):
+        self.set_y(-10)
         self.set_font('helvetica', size=9)
         self.cell(0,6,"""Ante cualquier duda, favor de comunicarse al Centro de Atención Telefónica de la SF al 01-800-3-10-40-10 donde """ ,ln=1,align="C")
         self.cell(0,2, "con gusto le atenderemos de Lunes a Viernes en horario de 8 a 15 horas, escríbanos al correo safcat@tabasco.gob.mx",align="C")
 #FIN footer
 #Body
-    def body(self,folio,importe,controlEstado,fechaEmision,fechaVencimiento,hora,concepto,splitlinea):
+    def body(self,folio,importe,controlEstado,fechaEmision,fechaVencimiento,hora,concepto,lineaCaptura,lineaCapturaOxxo):
         self.set_font('helvetica', size=12)
         self.cell(0,20,"Folio: " + folio,ln=1,align="R")
 
         qr = qrcode.QRCode(
                 version=1,
-                box_size=10,
+                box_size=15,
                 border=5)
         qr.add_data('Transacción|{}&Importe|{}'.format(folio,importe))
         qr.make(fit=True)
         img = qr.make_image(fill='black', back_color='white')
-        self.image(img.get_image(), x=180, y=50,h=20)
+        self.image(img.get_image(), x=165, y=45,h=40)
 
 
         self.set_font('helvetica', size=9)
@@ -295,12 +337,16 @@ C) Si requiere algún cambio en los datos de su vehículo o propietario [RFC, Co
         self.set_font('helvetica', size=13)
         #Lineas de Captura Banco
         self.cell(0,5,"Linea de captura para bancos " ,ln=1,align="C")
-        self.cell(0,5,splitlinea[0],ln=1,align="C")
+        self.cell(0,5,lineaCaptura,ln=1,align="C")
         svg_img_bytes = io.BytesIO()
-        Code128(splitlinea[0], writer=SVGWriter()).write(svg_img_bytes)
-        self.image(svg_img_bytes,80,158,45)
+        Code128(lineaCaptura, writer=SVGWriter()).write(svg_img_bytes)
+        self.image(svg_img_bytes,80,166,45)
+
         #Lineas de captura OXXO
-        self.ln(20)
+        svg_img_bytes2 = io.BytesIO()
+        Code128(lineaCapturaOxxo, writer=SVGWriter()).write(svg_img_bytes2)
+        self.image(svg_img_bytes2,10,214,60)
+        self.ln(30)
         self.set_font('helvetica', size=8)
         self.cell(0,5,"Línea de captura exclusiva de OXXO:" ,align="L")
         self.set_font('helvetica',"B", size=9)
@@ -312,13 +358,10 @@ C) Si requiere algún cambio en los datos de su vehículo o propietario [RFC, Co
         self.cell(0,5,"SANTANDER: CONVENIO =2527" ,ln=1,align="R")
         self.cell(0,5,"BANCOMER: CIE =0672505" ,ln=1,align="R")
         self.cell(0,5,"BANCO DEL BAJIO: SERVICIO 1108" ,ln=1,align="R")
-        self.image("./images/lugares.png",102,220,100)
-        self.image("./images/oxxo.jpg",25,185,20)
+        self.image("apilineacaptura/images/lugares.png",102,223,100)
+        self.image("apilineacaptura/images/oxxo.jpg",25,192,20)
         self.set_font('helvetica', size=13)
-        self.cell(0,-30,splitlinea[1],ln=1,align="L")
-        svg_img_bytes2 = io.BytesIO()
-        Code128(splitlinea[1], writer=SVGWriter()).write(svg_img_bytes2)
-        self.image(svg_img_bytes2,10,210,60)
+        
         self.ln(60)
         self.set_font('helvetica',"B", size=9)
         self.cell(0,5,"Su pago puede tardar hasta 72 horas hábiles en verse reflejado." ,ln=1,align="C")
@@ -331,15 +374,26 @@ C) Si requiere algún cambio en los datos de su vehículo o propietario [RFC, Co
 @app.route("/pagoRecaudador",methods=["POST"])
 def pagoRecaudador():
     try:
-        requestData=request.get_json()
-        dataEncriptada=encriptarData(requestData)
+               
+        data_request=request.get_json()
+        
+        user = data_request['user']
+        email = data_request['email']
+        auth=data_request['AuthS701']
+        reference=data_request['referenceKey']
+        access=data_request['AccessUser']
+        establish=data_request['EstablishNum']
+        branch=data_request['BranchSource']
 
-        #Generacion del token
-        user = requestData['user']
-        email = requestData['email']
+        data_recaudador=json.dumps({"AuthS701":auth, "referenceKey":reference, "AccessUser":access, "EstablishNum":establish, "BranchSource":branch}, separators=(',', ':'))
         data=json.dumps({"user":user, "email":email}, separators=(',', ':'))
-        tokenGenerado=str(generarToken(data))
-        #Fin de generacion del token
+        
+        tokenGenerado=generarToken(data)
+        
+        dataEncriptadaRecaudador=encriptarData(data_recaudador)
+        param=json.dumps({"query":dataEncriptadaRecaudador},separators=(',', ':'))
+
+        
 
         header={
                         'Content-Type':'application/json',
@@ -351,16 +405,26 @@ def pagoRecaudador():
                         'Authorization':'Bearer '+ tokenGenerado
                 }
 
-        dataEnviar=json.dumps({"data":dataEncriptada},separators=(',', ':'))
         
-        return dataEnviar
+        response=requests.post(url=rutaRecaudador+"?query="+param,headers=header)
+        encriptadoRecaudador=desencriptado(response.text)
+        dataDesencriptado= json.loads(encriptadoRecaudador)
+        logging.warning(dataDesencriptado)
+        
+        return dataDesencriptado
     except Exception as error:
      logging.warning(error)
 
 
+@app.route("/webhookSAIG",methods=["POST"])
+def webhook():
+    data=request.get_json()
+    return data
+
+
 if __name__=="__main__":
 
-    app.run(debug=True,port="4000")
-    #serve(app, host=ip_address, port=port)
+    #app.run(debug=True,port="4000")
+    serve(app, host=ip_address, port=port)
     
 
